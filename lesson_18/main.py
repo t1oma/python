092e7f2b94c11c83f2ecf5bcdf97a3ab36355e39
movavi_school <bodya.podkopaev@yandex.ru>
2023-02-01 20:59:31 +0700

    1 февраля

---
+++ lesson_18/main.py
@@ -0,0 +1,28 @@
+import easygui
+
+# easygui.msgbox(
+#     msg="Hello world",
+#     image="pictures/AnyConv.com__IMG_3774.png"
+#     title="Novosibirsk",
+#     ok_button='dadspada'
+# )
+
+# easygui.buttonbox(
+#     msg="Куда сам сядешь...",
+#     choices= ("Стул твёрдый", "Стул не твердый"),
+#     title="ИНтересная загадка"
+# )
+
+# number = easygui.integerbox(
+#     msg="За сколько купишь слона?",
+#     lowerbound=5,
+#     upperbound=101,
+#     default=25
+# )
+# print(number)
+
+# text = easygui.enterbox(
+#     msg="Как твоё имя, путник?",
+#     title="Диалог",
+#     default="Стас Борецкий"
+# )
